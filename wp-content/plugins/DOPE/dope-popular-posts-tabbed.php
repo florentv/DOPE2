7636725adbf7beb4e1e4bfcb09351af6b26df075
J-Loup <jeanloup.jamet@gmail.com>
2012-11-25 14:28:25 +0100

    dope populaire plugin

---
+++ wp-content/plugins/DOPE/dope-popular-posts-tabbed.php
@@ -0,0 +1,116 @@
+<?php
+/*
+Plugin Name: Dope Populaire
+Plugin URI: http://www.dealerofpeopleemotions.com
+Description: Affiche les articles les plus populaires - 3 onglets
+Author: J-Loup
+Version: 1
+*/
+ 
+ 
+class DopePopulaireWidget extends WP_Widget
+{
+  function DopePopulaireWidget()
+  {
+    $widget_ops = array('classname' => 'dope-populaire-widget');
+    $this->WP_Widget('DopePopulaireWidget', 'DOPE populaire', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'titleLength' => 25 ) );
+    $title = $instance['title'];
+    $titleLength = $instance['titleLength'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Titre du widget : <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label>
+  	<label for="<?php echo $this->get_field_id('titleLength'); ?>">Longueur des titres: <input class="widefat" id="<?php echo $this->get_field_id('titleLength'); ?>" name="<?php echo $this->get_field_name('titleLength'); ?>" type="text" value="<?php echo attribute_escape($titleLength); ?>" /></label>
+  </p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    $instance['titleLength'] = $new_instance['titleLength'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+    $titleLength = empty($instance['titleLength']) ? 20 : $instance['titleLength'];
+    
+    $args = array(
+    	'title' => __('Popular Posts', 'wordpress-popular-posts'),
+    	'limit' => 5,
+    	'range' => 'daily',
+    	'order_by' => 'views',
+    	'post_type' => 'post',
+    	'author' => '',
+    	'cat' => '',
+    	'shorten_title' => array(
+    		'active' => true,
+    		'length' => $titleLength,
+    		'keep_format' => false
+    	),
+    	'post-excerpt' => array(
+    		'active' => false,
+    		'length' => 55
+    	),				
+    	'thumbnail' => array(
+    		'active' => true,
+    		'width' => 10,
+    		'height' => 10
+    	),
+    	'rating' => false,
+    	'stats_tag' => array(
+    		'comment_count' => false,
+    		'views' => false,
+    		'author' => false,
+    		'date' => array(
+    			'active' => false,
+    			'format' => 'F j, Y'
+    		)
+    	),
+    	'markup' => array(
+    		'custom_html' => false,
+    		'wpp-start' => '&lt;ul&gt;',
+    		'wpp-end' => '&lt;/ul&gt;',
+    		'post-start' => '&lt;li&gt;',
+    		'post-end' => '&lt;/li&gt;',
+    		'title-start' => '&lt;h2&gt;',
+    		'title-end' => '&lt;/h2&gt;',
+    		'pattern' => array(
+    			'active' => false,
+    			'form' => '{image} {title}: {summary} {stats}'
+    		)
+    	)
+    );
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;?>
+ 	<ul class="dope-populaire-control">
+ 		<li>RÉCENT</li>
+ 		<li>30 JOURS</li>
+ 		<li>ALL-TIME</li>
+ 	</ul>
+ 	<div class="main container"> <?php
+ 	$args['range'] = "weekly" ;
+ 	wpp_get_mostpopular($args)
+ 	$args['range'] = "monthly" ;
+ 	wpp_get_mostpopular($args)
+ 	$args['range'] = "all" ;
+ 	wpp_get_mostpopular($args)
+ 	?>
+ 	</div>
+   <?php
+ 
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("DopeAleatoireWidget");') );?>
