39b71f282e5bdc08bd6917769012ef9f9685ca26
florentv <vie.florent@gmail.com>
2012-11-27 23:35:34 +0100

    test

--- wp-content/plugins/tabber-widget/lib/classes/js/it-core-class.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(
-	function() {
-		jQuery("form:not(.no-auto-focus) :input:not(.no-auto-focus):not(:checkbox):not(:button):not(:submit):visible:enabled:first").focus();
-	}
-);
